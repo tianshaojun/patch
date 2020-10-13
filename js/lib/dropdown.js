d95f25a17f7912455456069418e3861549a5a6d9
田少军 <tianshaojun@tianshaojundeMacBook-Pro.local>
2020-10-13 21:58:53 +0800

    1013晚上开发

---
+++ js/lib/dropdown.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	/* Slide Toogle */
+	$("ul.expmenu li > div.header").click(function(){
+		var arrow = $(this).find("span.arrow");
+
+		if(arrow.hasClass("up")){
+		     arrow.removeClass("up");
+		     arrow.addClass("down");
+		 }else if(arrow.hasClass("down")){
+		    arrow.removeClass("down");
+		    arrow.addClass("up");
+                          }
+
+		$(this).parent().find("ul.menu").slideToggle();
+	});
+});
